2114f5b12df250c75704cbf6c2932215c58223bf
Utkarsha Manoj <74818779+U-m245@users.noreply.github.com>
2023-05-26 06:27:11 +0530

    Update quicksort.scala

--- quicksort.scala
+++ quicksort.scala
@@ -1,13 +1,23 @@
-object ImperativeQuickSort {
+object ScalaSort {
+  def sort(a: List[Int]): List[Int] = {
+    if (a.length < 2) a
+    else {
+      val pivot = a(a.length / 2)
+      val smaller = a.filter(_ < pivot)
+      val equal = a.filter(_ == pivot)
+      val greater = a.filter(_ > pivot)
+      sort(smaller) ::: equal ::: sort(greater)
+    }
+  }
 
-  
-  def sort(a: Array[Int]) {
-
-    def swap(i: Int, j: Int) {
-      val t = a(i); a(i) = a(j); a(j) = t
+  def sortArray(a: Array[Int]): Unit = {
+    def swap(l: Int, r: Int): Unit = {
+      val t = a(l)
+      a(l) = a(r)
+      a(r) = t
     }
 
-    def sort1(l: Int, r: Int) {
+    def sort2(l: Int, r: Int): Unit = {
       val pivot = a((l + r) / 2)
       var i = l
       var j = r
@@ -20,28 +30,22 @@ object ImperativeQuickSort {
           j -= 1
         }
       }
-      if (l < j) sort1(l, j)
-      if (j < r) sort1(i, r)
+      if (l < j) sort2(l, j)
+      if (i < r) sort2(i, r)
     }
 
-    if (a.length > 0)
-      sort1(0, a.length - 1)
+    if (a.length > 1) sort2(0, a.length - 1)
   }
 
-  def println(ar: Array[Int]) {
-    def print1 = {
-      def iter(i: Int): String =
-        ar(i) + (if (i < ar.length-1) "," + iter(i+1) else "")
-      if (ar.length == 0) "" else iter(0)
-    }
-    Console.println("[" + print1 + "]")
+  def main(args: Array[String]): Unit = {
+    var xs = List(6, 2, 9, 8, 1)
+    println("Sorted List using Functional style:")
+    println(sort(xs))
+    
+    val xs2 = Array(6, 2, 9, 8, 1)
+    println("Sorted List using Imperative style:")
+    sortArray(xs2)
+    for (ele <- xs2)
+      println(ele)
   }
-
-  def main(args: Array[String]) {
-    var ar = Array(6,5,2,1,8);
-    println(ar)
-    sort(ar)
-    println(ar)
-  }
-
 }
